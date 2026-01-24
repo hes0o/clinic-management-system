82365bc031fe519ed849893cec36665879b8db16
Hassan Chawa <hassanchawa30@gmail.com>
2026-01-24 22:34:04 +0300

    Add core domain entities and business rules

---
+++ backend/HealthCenter.Domain/Enums/QueueStatus.cs
@@ -0,0 +1,10 @@
+namespace HealthCenter.Domain.Enums;
+
+public enum QueueStatus
+{
+    Waiting,
+    Called,
+    Missed,
+    Completed
+    // this is the 4 instances of the queue status so yeah
+}
